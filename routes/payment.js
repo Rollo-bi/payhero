a9543d74417b39495eb5dd624bf1b998768abc7a
Reuel kipkoech <57501208+Rollo-bi@users.noreply.github.com>
2026-06-28 16:43:16 +0300

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-    initiatePayment,
-    paymentCallback,
-    checkStatus
-} = require("../paymentController");
+const controller = require("../paymentController");
 
-router.post("/initiate", initiatePayment);
-router.post("/callback", paymentCallback);
-router.get("/status/:id", checkStatus);
+console.log("Controller:", controller);
+console.log("initiatePayment =", typeof controller.initiatePayment);
+console.log("paymentCallback =", typeof controller.paymentCallback);
+console.log("checkStatus =", typeof controller.checkStatus);
+
+router.post("/initiate", controller.initiatePayment);
+router.post("/callback", controller.paymentCallback);
+router.get("/status/:id", controller.checkStatus);
 
 module.exports = router;
